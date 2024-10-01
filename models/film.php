3bb25baa1bd520b832a59e42429a04cc41480d4a
mmantle-hud <m.e.mantle@hud.ac.uk>
2024-10-01 08:42:20 +0100

    Update film.php

--- models/film.php
+++ models/film.php
@@ -3,7 +3,7 @@
 function getConnection()
 {
 	try {
-		$conn = new PDO('mysql:host=localhost;dbname=CHT2520', 'CHT2520', 'tutor');
+		$conn = new PDO('mysql:host=localhost;dbname=MyDatabase', 'MyUsername', 'MyPassword');
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch (PDOException $exception) {
 		echo "Oh no, there was a problem" . $exception->getMessage();
